d57cd1befc416b3ca7d421f0d0dbf941547d1651
Samuel Louissaint <samuellclermont@gmail.com>
2020-11-03 20:56:39 +0100

    page d'accueil

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController {
+
+    /** 
+     * @Route("/", name="homepage")
+    */
+
+    public function home(){
+            return $this->render(
+                'home.html.twig',
+                 [ 'title' => "Bonjour à tous" ]
+        );
+    }
+}
+
+?>
